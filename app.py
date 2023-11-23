d3c2be8363815495b8cc2f857dbc4c2b92a93cb7
thwjdstar <151480728+thwjdstar@users.noreply.github.com>
2023-11-23 11:46:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import seaborn as sb
 
 def main():
 
-    st.title('자동차 데이터 분석')
+    st.title('자동차 데이터 분석~')
 
     st.subheader('차트 보기')
-    
+
     df = pd.read_csv('./data/fuel_econ.csv')
 
     if st.checkbox('데이터 프레임 보기') :
